2d40679a12e445bb4b17e535956cad419c2e2f7a
making <makingx@gmail.com>
2016-09-28 18:39:11 +0900

    Update

--- src/main/java/mrs/ReservationApplication.java
+++ src/main/java/mrs/ReservationApplication.java
@@ -1,6 +1,9 @@
 package mrs;
 
+import mrs.domain.model.MeetingRoom;
+import mrs.domain.model.ReservableRoom;
 import mrs.domain.model.ReservableRoomId;
+import mrs.domain.model.Reservation;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
@@ -10,16 +13,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.support.ConfigurableConversionService;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.interceptor.*;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -28,6 +34,33 @@ public class ReservationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationApplication.class, args);
+	}
+
+	@Profile("!cloud")
+	@Bean
+	CommonsRequestLoggingFilter requestDumperFilter() {
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter() {
+			@Override
+			protected boolean shouldLog(HttpServletRequest request) {
+				return logger.isInfoEnabled();
+			}
+
+			@Override
+			protected void beforeRequest(HttpServletRequest request, String message) {
+				logger.info(message);
+			}
+
+			@Override
+			protected void afterRequest(HttpServletRequest request, String message) {
+				logger.info(message);
+			}
+		};
+		filter.setIncludePayload(true);
+		filter.setIncludeClientInfo(true);
+		filter.setIncludeHeaders(false);
+		filter.setIncludeQueryString(true);
+		filter.setMaxPayloadLength(2048000);
+		return filter;
 	}
 
 	@Configuration
@@ -41,6 +74,13 @@ public class ReservationApplication {
 					s -> LocalTime.parse(s, DateTimeFormatter.ISO_LOCAL_TIME));
 			conversionService.addConverter(String.class, ReservableRoomId.class,
 					ReservableRoomId::valueOf);
+		}
+
+		@Override
+		public void configureRepositoryRestConfiguration(
+				RepositoryRestConfiguration config) {
+			config.exposeIdsFor(ReservableRoom.class, MeetingRoom.class,
+					Reservation.class);
 		}
 	}
 
