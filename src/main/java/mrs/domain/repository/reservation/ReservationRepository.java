94a2596bfc64770fde968dbb619a3d680730c93b
Toshiaki Maki <makingx@gmail.com>
2016-12-19 22:30:31 -1000

    Refactor REST Resource Path

--- src/main/java/mrs/domain/repository/reservation/ReservationRepository.java
+++ src/main/java/mrs/domain/repository/reservation/ReservationRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import mrs.domain.model.ReservableRoomId;
 import mrs.domain.model.Reservation;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
+	@RestResource(rel = "findByReservableRoomId", path = "findByReservableRoomId")
 	List<Reservation> findByReservableRoom_ReservableRoomIdOrderByStartTimeAsc(
 			@Param("reservableRoomId") ReservableRoomId reservableRoomId);
 }
--- src/main/java/mrs/domain/repository/room/ReservableRoomRepository.java
+++ src/main/java/mrs/domain/repository/room/ReservableRoomRepository.java
@@ -11,12 +11,15 @@ import org.springframework.data.jpa.repository.Lock;
 import mrs.domain.model.ReservableRoom;
 import mrs.domain.model.ReservableRoomId;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface ReservableRoomRepository
 		extends JpaRepository<ReservableRoom, ReservableRoomId> {
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@RestResource(exported = false)
 	ReservableRoom findOneForUpdateByReservableRoomId(ReservableRoomId reservableRoomId);
 
+	@RestResource(rel = "findByReservedDate", path = "findByReservedDate")
 	List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(
 			@Param("reservedDate") LocalDate reservedDate);
 }
